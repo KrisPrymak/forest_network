37cbccf635dc89726fb29c4b572d946db8d1f565
Kristina Priimak <krispriimak@air-kristina.local>
2022-12-05 09:51:14 +0300

    dialog page layout

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,35 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Kesha
+                </div>
+                <div className={s.dialog}>
+                    Lalka
+                </div>
+                <div className={s.dialog}>
+                    Musya
+                </div>
+                <div className={s.dialog}>
+                    Kiska
+                </div>
+                <div className={s.dialog}>
+                    Gosha
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    hello, how are you
+                </div>
+                <div className={s.message}>
+                   Yo yo yo
+                </div>
+                <div className={s.message}>
+                    lalalal lolo
+                </div>
+            </div>
         </div>
     );
 };
