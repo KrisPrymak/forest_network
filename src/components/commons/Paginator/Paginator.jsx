7c9d874a5afa11ab58e6de7cbc032e4d9cdd0b87
KrisPrymak <kriskiwinkaa@gmail.com>
2022-12-24 11:25:38 +0300

    93 paginator

--- src/components/commons/Paginator/Paginator.jsx
+++ src/components/commons/Paginator/Paginator.jsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './../../Users/Users.module.css';
+import cn from 'classnames';
 
-const Paginator = ({currentPage, onPageChanged, totalUsersCount, pageSize}) => {
+const Paginator = ({currentPage, onPageChanged, totalUsersCount, pageSize, portionSize = 10}) => {
     let pagesCount = Math.ceil(totalUsersCount / pageSize);
-  let pages = [];
+    let pages = [];
 
   for (let i = 1; i <= pagesCount; i++) {
     pages.push(i);
   }
 
-  if(pages.length > 80) {
-    let newP = pages.reverse().slice(0, 80);
-    pages = newP;
-  }
+  let portionCount = Math.ceil(pagesCount / portionSize);
+  let [portionNumber, setportionNumber] = useState(1);
+  let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+  let rightPortionPageNumber = portionNumber * portionSize;
+ 
+
 
     return (
         <div className={s.pages}>
-        {pages.map((p) => {
+          {portionNumber > 1 && 
+          <button className={s.previous}  onClick={() => {setportionNumber(portionNumber - 1)}}>
+            <img src="https://cdn-icons-png.flaticon.com/512/570/570220.png" alt="prev" />
+        </button>}
+        {pages
+          .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+          .map((p) => {
           return (
             <span 
               className={currentPage === p && s.selectedPage}
@@ -28,6 +37,9 @@ const Paginator = ({currentPage, onPageChanged, totalUsersCount, pageSize}) => {
             </span>
           );
         })}
+        { portionCount > portionNumber && <button className={s.next} onClick={() => {setportionNumber(portionNumber + 1)}}>
+          <img src="https://cdn-icons-png.flaticon.com/512/318/318275.png" alt="next" />
+          </button>}
       </div>
     );
 };
